0486e0195762c6e5adaa9a8ce17657119d77e29f
BlitzKraig <craig@blitzkraig.com>
2020-06-28 23:08:10 +0100

    Added new input mode to allow table pasting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,21 @@
+## 1.2.0 - Added new input mode (2020/06/28)
+
+* Change input mode in module settings
+* New input mode should allow you to directly copy entries from tables found online following the format `[number|number-range][whitespace]Entry Text Here[newline]`
+
+eg.
+1	Entry number one
+2-3	Entry number two
+4	Entry number 3
+
+The whitespace can be either a single space, or a single tab.
+
 ## 1.1.0 - Added table weights (2020/06/28)
+
 * Entries can now be 'weighted' with {} - e.g Entry1,Entry2{2},Entry3 will create a 1d4 table where Entry2 is selected on a roll of 2 or 3
 
 ## 1.0.0 - Initial public release (2020/06/17)
+
 * Added the ability to change the separator character.
 * Updated settings commits to remember the previously used separator
 * Squashed some bugs
--- easytable.js
+++ easytable.js
@@ -1,15 +1,55 @@
 Hooks.on("renderSidebarTab", async (app, html) => {
     if (app.options.id == "tables") {
         let button = $("<button class='new-easytable'><i class='fas fa-file-csv'></i> New EasyTable</button>")
-        let settings = game.settings.get("easytable", "tableSettings")
+        let tablePasteMode = game.settings.get("EasyTable", "tablePasteMode");
+        let settings = game.settings.get("EasyTable", "tableSettings")
         let title = settings.title;
         let description = settings.description;
         let csvData = settings.data;
         let separator = settings.separator;
         button.click(function () {
-            new Dialog({
-                title: "EasyTable",
-                content: `<div>
+            if (tablePasteMode) {
+                new Dialog({
+                    title: "EasyTable Table Paste Mode",
+                    content: `<div> 
+                    <div class="form-group"><div>Table Title</div><input type='text' name="tableTitle" value="${title}"/></div>
+                    <div class="form-group"><div>Table Description</div><input type='text' name="tableDescription" value="${description}"/></div>
+                    <div class="form-group" title="Paste your table data here"><div>Table Data</div><textarea name="tableData">1\tValue1\n2\tValue2\n3-4\tValue3</textarea></div>
+                    <hr/>
+                </div>
+                `,
+                    buttons: {
+                        generate: {
+                            label: "Generate",
+                            callback: async (html) => {
+                                let title = html.find('[name="tableTitle"]').val();
+                                let description = html.find('[name="tableDescription"]').val();
+                                let tableData = html.find('[name="tableData"]').val();
+
+                                //TODO: Notify while dialog is still open, allowing changes
+                                if (!title) {
+                                    ui.notifications.error("EasyTables require a Title");
+                                    return;
+                                } else if (!tableData) {
+                                    ui.notifications.error("EasyTables require the Table Data field to be filled");
+                                    return;
+                                }
+
+                                await EasyTable.generateTablePastedData(title, description, tableData);
+
+                                ui.notifications.notify(`EasyTable ${title} created`);
+                            }
+                        },
+                        cancel: {
+                            label: "Cancel"
+                        }
+                    },
+                    default: "generate"
+                }).render(true);
+            } else {
+                new Dialog({
+                    title: "EasyTable",
+                    content: `<div>
          <div class="form-group"><div>Table Title</div><input type='text' name="tableTitle" value="${title}"/></div>
          <div class="form-group"><div>Table Description</div><input type='text' name="tableDescription" value="${description}"/></div>
          <div class="form-group" title="Paste your CSV data here"><div>CSV Data</div><textarea name="csv">${csvData}</textarea></div>
@@ -17,46 +57,48 @@ Hooks.on("renderSidebarTab", async (app, html) => {
          <hr/>
         </div>
         `,
-                buttons: {
-                    generate: {
-                        label: "Generate",
-                        callback: async (html) => {
-                            let title = html.find('[name="tableTitle"]').val();
-                            let description = html.find('[name="tableDescription"]').val();
-                            let csvData = html.find('[name="csv"]').val();
-                            let separator = html.find('[name="separator"]').val();
+                    buttons: {
+                        generate: {
+                            label: "Generate",
+                            callback: async (html) => {
+                                let title = html.find('[name="tableTitle"]').val();
+                                let description = html.find('[name="tableDescription"]').val();
+                                let csvData = html.find('[name="csv"]').val();
+                                let separator = html.find('[name="separator"]').val();
 
-                            //TODO: Notify while dialog is still open, allowing changes
-                            if (!title) {
-                                ui.notifications.error("EasyTables require a Title");
-                                return;
-                            } else if (!csvData) {
-                                ui.notifications.error("EasyTables require the CSV Data field to be filled");
-                                return;
-                            } else if (!separator || separator.length > 1) {
-                                //TODO: Restrict this properly
-                                ui.notifications.error("EasyTables require the separator field to contain a single character");
-                                return;
+                                //TODO: Notify while dialog is still open, allowing changes
+                                if (!title) {
+                                    ui.notifications.error("EasyTables require a Title");
+                                    return;
+                                } else if (!csvData) {
+                                    ui.notifications.error("EasyTables require the CSV Data field to be filled");
+                                    return;
+                                } else if (!separator || separator.length > 1) {
+                                    //TODO: Restrict this properly
+                                    ui.notifications.error("EasyTables require the separator field to contain a single character");
+                                    return;
+                                }
+                                //TODO: Improve settings update
+                                game.settings.set("easytable", "tableSettings", {
+                                    title: 'EasyTable',
+                                    description: 'An easy table',
+                                    data: `val1${separator}val2${separator}val3`,
+                                    separator: separator || ','
+                                });
+                                await EasyTable.generateTable(title, description, csvData, separator);
+
+                                ui.notifications.notify(`EasyTable ${title} created`);
                             }
-                            //TODO: Improve settings update
-                            game.settings.set("easytable", "tableSettings", {
-                                title: 'EasyTable',
-                                description: 'An easy table',
-                                data: `val1${separator}val2${separator}val3`,
-                                separator: separator || ','
-                            });
-                            await EasyTable.generateTable(title, description, csvData, separator);
-
-                            ui.notifications.notify(`EasyTable ${title} created`);
+                        },
+                        cancel: {
+                            label: "Cancel"
                         }
                     },
-                    cancel: {
-                        label: "Cancel"
-                    }
-                },
-                default: "generate"
-            }).render(true);
+                    default: "generate"
+                }).render(true);
+            }
         })
+
         html.find(".directory-footer").append(button);
     }
 })
@@ -68,13 +110,38 @@ Hooks.on("init", () => {
         data: 'val1,val2{2},val3',
         separator: ','
     };
-    game.settings.register("easytable", "tableSettings", {
+    game.settings.register("EasyTable", "tableSettings", {
         name: "Easytable Default Settings",
         scope: "world",
         config: false,
         default: etSettings
     });
-    game.settings.set("easytable", "tableSettings", etSettings);
+    game.settings.set("EasyTable", "tableSettings", etSettings);
+    game.settings.register("EasyTable", "tablePasteMode", {
+        name: "Paste from table mode",
+        hint: "Changing this will refresh your page! Import using data copied straight from a table. Each entry should be on its own line. Each entry should begin with the dice number (eg. 1 or 1-3) followed by a space or tab, followed by the entry data",
+        scope: "client",
+        config: true,
+        default: false,
+        type: Boolean,
+        onChange: value => {
+            window.location.reload();
+        }
+    })
+
+
+    //     01–50	Potion of healing
+    // 51–60	Spell scroll (cantrip)
+    // 61–70	Potion of climbing
+    // 71–90	Spell scroll (1st level)
+    // 91–94	Spell scroll (2nd level)
+    // 95–98	Potion of greater healing
+    // 99	Bag of holding
+    // 00	Driftglobe
+    //     1	One of your parents is a lycanthrope, and you’ve inherited some of the curse.
+    // 2	You are descended from a legendary druid, a fact manifested by your ability to partially change shape.
+    // 3	A fey spirit gifted you with the ability to adopt different bestial aspects.
+    // 4	An ancient animal spirit dwells within you, allowing you to walk this path.
 });
 
 
@@ -90,6 +157,47 @@ class EasyTable {
                 weight = weight.split('}')[0];
             }
             console.log(weight);
+            resultsArray.push({
+                "type": 0,
+                "text": text,
+                "weight": weight || 1,
+                "drawn": false
+            });
+        });
+        let table = await RollTable.create({
+            name: title,
+            description: description,
+            results: resultsArray,
+            replacement: true,
+            displayRoll: true
+        });
+        await table.normalize();
+    }
+
+    static async generateTablePastedData(title, description, tableData) {
+
+        let resultsArray = [];
+        let tableEntries = tableData.split(/[\r\n]+/);
+        tableEntries.forEach((tableEntry, i) => {
+            let weight, text;
+            if (tableEntry.match(/^\d/)) {
+                [weight, text] = tableEntry.split(/(?<=^\S+)\s/)
+                try {
+                    if (weight.match(/[\d]+-[\d]+/)) {
+                        console.log("TEST");
+                        weight = weight.split('-')[1] - weight.split('-')[0] + 1;
+                    } else if (weight.match(/[\d]+–[\d]+/)) { // Not actually a hyphen...
+                        weight = weight.split('–')[1] - weight.split('–')[0] + 1;
+                    } else {
+                        weight = 1;
+                    }
+                } catch (e) {
+                    weight = 1;
+                }
+            }
+            if(!text){
+                text = "TEXT MISSING";
+            }
             resultsArray.push({
                 "type": 0,
                 "text": text,
